a46c9c58bc4195478db55ad9a087e01383673b6a
Brian Hoff <blhoff52002@gmail.com>
2019-10-18 12:54:27 -0500

    Updates

--- precache-manifest.00d9aa3d6b981240cf6c8f68bdb7931b.js
+++ precache-manifest.00d9aa3d6b981240cf6c8f68bdb7931b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "811839a90d86d95126700c61895fbd33",
+    "revision": "dd3108ac22fde1587b36c955e5728e34",
     "url": "/clicky-game/index.html"
   },
   {
-    "revision": "c27572c1ed01c352bb1e",
+    "revision": "bf6ba641d99f15da36d6",
     "url": "/clicky-game/static/css/main.1a369853.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/clicky-game/static/js/2.03cba8c6.chunk.js"
   },
   {
-    "revision": "c27572c1ed01c352bb1e",
-    "url": "/clicky-game/static/js/main.59195527.chunk.js"
+    "revision": "bf6ba641d99f15da36d6",
+    "url": "/clicky-game/static/js/main.de4d9621.chunk.js"
   },
   {
     "revision": "9e63f44ec9a3dfc4c648",
