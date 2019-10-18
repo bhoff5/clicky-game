5be56e3eef2616fedb9c70fc853b966fdbeef9fd
Brian Hoff <blhoff52002@gmail.com>
2019-10-17 21:00:32 -0500

    fixing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Main from "./pages/Main";
-import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
 function App() {
